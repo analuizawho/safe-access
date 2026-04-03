b3248dd24eb2ad5984b6679d14f43e40a8aef323
analuizawho <analuiza319@hotmail.com>
2026-04-03 19:25:50 -0300

    feat: add Role attribute to Admin

--- user-service/src/main/java/com/analuizawho/safe_access/user_service/entities/Admin.java
+++ user-service/src/main/java/com/analuizawho/safe_access/user_service/entities/Admin.java
@@ -1,14 +1,31 @@
 package com.analuizawho.safe_access.user_service.entities;
 
 import com.analuizawho.safe_access.user_service.dtos.UpdateAdminDTO;
+import com.analuizawho.safe_access.user_service.entities.enums.Department;
+import com.analuizawho.safe_access.user_service.entities.enums.Role;
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "admin")
 public class Admin extends User{
 
+    private Role role;
+
     public Admin(){
         super();
+    }
+
+    public Admin(Long id, String name, Department department, String password, Boolean userActive, Role role) {
+        super(id, name, department, password, userActive);
+        this.role = role;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public void updateAdminInfo(UpdateAdminDTO infoData){
---
+++ user-service/src/main/java/com/analuizawho/safe_access/user_service/entities/enums/Role.java
@@ -0,0 +1,6 @@
+package com.analuizawho.safe_access.user_service.entities.enums;
+
+public enum Role {
+    SUPER_ADMIN,
+    ADMIN,
+}
